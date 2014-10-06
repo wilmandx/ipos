868ba1dd4b53edda501d8aa064ac2a3c55545d03
Hector Naranjo Gallego <hnaranjog@gmail.com>
2014-10-05 19:27:06 -0500

    Modificaciones template  ventas y pedidos
    Se realiza modificaciones en el template como formularios tablas etc,
    con diseño bootstrap

--- ventas/urls.py
+++ ventas/urls.py
@@ -5,5 +5,5 @@ from ventas import views
 urlpatterns = patterns('',
     url(r'^$', views.venta_desktop, name='venta_desktop'),
     url(r'^mobile/$', views.venta_mobile, name='venta_mobile'),
-    #url(r'^listar/$', views.listar, name='listar'),
+    url(r'^listar/$', views.listar, name='listar'),
 )
--- ventas/views.py
+++ ventas/views.py
@@ -9,9 +9,13 @@ def venta_desktop(request):
     context = {'message':'ok'}
     return render(request, 'venta.html',context)
 
-
 @login_required
 def venta_mobile(request):
 	list_categories=ValorTipo.objects.filter(padre=1)
 	context = {'message':'ok','list_categories':list_categories}
 	return render(request, 'mobile/venta.html',context)
+
+@login_required
+def listar(request):
+    context = {'message':'ok'}
+    return render(request, 'listar/pedido.html',context)
