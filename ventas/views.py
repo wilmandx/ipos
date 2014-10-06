427ef0d7bca8a1ea420917e7e742b81a351e30df
Wilman <wilman@utp.edu.co>
2014-10-06 10:08:40 -0500

    Lista de productos

--- ventas/views.py
+++ ventas/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from gestion.models import ValorTipo
+from gestion.models import ValorTipo,Producto
 
 # Create your views here.
+class Categoria:
+	listProductos=[]
 
 @login_required
 def venta_desktop(request):
@@ -11,8 +13,22 @@ def venta_desktop(request):
 
 @login_required
 def venta_mobile(request):
+	list_cate_prod={}
 	list_categories=ValorTipo.objects.filter(padre=1)
-	context = {'message':'ok','list_categories':list_categories}
+	list_productos=Producto.objects.all()
+	for producto in list_productos:
+		for categoria in list_categories:
+			if categoria.id==producto.tipoCategoria.id :
+				if categoria.id in list_cate_prod :
+					temp=list_cate_prod[categoria.id]
+				else :
+					temp=Categoria()
+					temp.listProductos=[]
+				temp.tipoCategoria=categoria
+				temp.listProductos.append(producto)
+				list_cate_prod[categoria.id]=temp
+
+	context = {'message':'ok','list_categories':list_cate_prod}
 	return render(request, 'mobile/venta.html',context)
 
 @login_required
